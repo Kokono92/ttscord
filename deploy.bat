9d98f4559f801a32d54ce02a120243cec77f0408
Kokono92 <Kokono92n@gmail.com>
2026-05-20 00:40:23 +0900

    ビルドに設定を組み込む

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 flutter pub get
-flutter pub global run peanut -b web-pages --release -m "built web by peanut"
+flutter pub global run peanut -b web-pages --release -m "built web by peanut" --extra-args "--dart-define-from-file=./config.json"
